d59e32e54f4fc76e4a053ec0747bb9f8576321a3
Redenfloyd <floydcayanan12@gmail.com>
2018-01-02 21:12:26 +0800

    updated DB

--- dinedb.sql
+++ dinedb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 25, 2017 at 06:50 AM
+-- Generation Time: Jan 02, 2018 at 02:10 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -25,6 +25,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Stand-in structure for view `cart`
+-- (See below for the actual view)
+--
+CREATE TABLE `cart` (
+`order_item_id` int(11)
+,`order_item_qty` int(11)
+,`order_item_subtotal` float
+,`product_id` int(11)
+,`product_image` varchar(30)
+,`product_name` varchar(30)
+,`product_description` varchar(50)
+,`product_price` float
+,`ordered_id` int(11)
+,`ordered_time` timestamp
+,`ordered_qr_code` int(11)
+,`ordered_total` float
+);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `guest`
 --
 
@@ -42,7 +63,7 @@ CREATE TABLE `ordered` (
   `ordered_id` int(11) NOT NULL,
   `ordered_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `ordered_qr_code` int(11) NOT NULL,
-  `ordered_total` float NOT NULL,
+  `ordered_total` float NOT NULL DEFAULT '0',
   `ordered_guest_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -55,7 +76,7 @@ CREATE TABLE `ordered` (
 CREATE TABLE `order_item` (
   `order_item_id` int(11) NOT NULL,
   `order_item_qty` int(11) NOT NULL,
-  `order_item_subtotal` float NOT NULL,
+  `order_item_subtotal` float NOT NULL DEFAULT '0',
   `order_item_product_id` int(11) NOT NULL,
   `order_item_ordered_id` int(11) NOT NULL,
   `order_item_status` enum('ACTIVE','INACTIVE') NOT NULL DEFAULT 'ACTIVE'
@@ -74,7 +95,7 @@ CREATE TABLE `product` (
   `product_description` varchar(50) NOT NULL,
   `product_price` float NOT NULL,
   `product_availability` enum('AVAILABLE','NOT AVAILABLE') NOT NULL,
-  `product_category` enum('MEALS','DRINKS','DESSERTS','EXTRAS') NOT NULL,
+  `product_category` enum('DRINKS','RICE MEAL','SOUP','MAIN COURSE','EXTRAS') NOT NULL,
   `product_created_by` int(11) NOT NULL,
   `product_created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `product_modified_by` int(11) NOT NULL,
@@ -90,10 +111,10 @@ CREATE TABLE `product` (
 CREATE TABLE `receipt` (
   `receipt_id` int(11) NOT NULL,
   `receipt_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `receipt_total` float NOT NULL,
+  `receipt_total` float NOT NULL DEFAULT '0',
   `receipt_cashier` int(11) NOT NULL,
-  `receipt_cash` float NOT NULL,
-  `receipt_change` float NOT NULL
+  `receipt_cash` float NOT NULL DEFAULT '0',
+  `receipt_change` float NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -104,7 +125,7 @@ CREATE TABLE `receipt` (
 
 CREATE TABLE `receipt_item` (
   `receipt_item_id` int(11) NOT NULL,
-  `receipt_item_subtotal` float NOT NULL,
+  `receipt_item_subtotal` float NOT NULL DEFAULT '0',
   `receipt_item_quantity` int(11) NOT NULL,
   `receipt_item_product_id` int(11) NOT NULL,
   `receipt_item_receipt_id` int(11) NOT NULL,
@@ -139,6 +160,15 @@ INSERT INTO `user` (`user_id`, `user_first_name`, `user_mi`, `user_last_name`, `
 (17000, 'Redenfloyd Gabrielle', 'M', 'Cayanan', 'BA3253876AED6BC22D4A6FF53D8406C6AD864195ED144AB5C87621B6C233B548BAEAE6956DF346EC8C17F5EA10F35EE3CBC514797ED7DDD3145464E2A0BAB413', 'SUPERADMIN', 'ACTIVE', 1, '2017-12-24 10:40:32', 1, '2017-12-24 11:05:43'),
 (17001, 'Joanne', 'S', 'Malaluan', 'BA3253876AED6BC22D4A6FF53D8406C6AD864195ED144AB5C87621B6C233B548BAEAE6956DF346EC8C17F5EA10F35EE3CBC514797ED7DDD3145464E2A0BAB413', 'ADMIN', 'ACTIVE', 1, '2017-12-24 11:06:25', 1, '2017-12-24 11:06:25'),
 (17002, 'Angela', 'C', 'Ong', 'BA3253876AED6BC22D4A6FF53D8406C6AD864195ED144AB5C87621B6C233B548BAEAE6956DF346EC8C17F5EA10F35EE3CBC514797ED7DDD3145464E2A0BAB413', 'REGULAR', 'ACTIVE', 1, '2017-12-24 11:07:08', 1, '2017-12-24 11:07:08');
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `cart`
+--
+DROP TABLE IF EXISTS `cart`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `cart`  AS  select `order_item`.`order_item_id` AS `order_item_id`,`order_item`.`order_item_qty` AS `order_item_qty`,`order_item`.`order_item_subtotal` AS `order_item_subtotal`,`product`.`product_id` AS `product_id`,`product`.`product_image` AS `product_image`,`product`.`product_name` AS `product_name`,`product`.`product_description` AS `product_description`,`product`.`product_price` AS `product_price`,`ordered`.`ordered_id` AS `ordered_id`,`ordered`.`ordered_time` AS `ordered_time`,`ordered`.`ordered_qr_code` AS `ordered_qr_code`,`ordered`.`ordered_total` AS `ordered_total` from ((`order_item` join `product` on((`order_item`.`order_item_product_id` = `product`.`product_id`))) join `ordered` on((`order_item`.`order_item_ordered_id` = `ordered`.`ordered_id`))) ;
 
 --
 -- Indexes for dumped tables
