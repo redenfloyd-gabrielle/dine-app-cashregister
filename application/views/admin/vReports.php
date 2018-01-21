ce5f775153253ce7077bcb1468fc58ec624bac55
Angela Ong <gelaiong18@gmail.com>
2018-01-21 16:02:56 +0800

    Reports UI

--- application/views/admin/vReports.php
+++ application/views/admin/vReports.php
@@ -1,5 +1,5 @@
 <div class="pusher">
-    <div class='ui hidden segment'></div>
+    <div class='ui hidden divider'></div>
     <div class='ui padded segment'>
         <!-- header -->
         <div class='ui basic segment'>
@@ -629,93 +629,141 @@
 
         <!-- content -->
         <div class='ui segments'>
-            <div class='ui basic segment'> 
-                <h5 class='ui header brown ribbon label'><i class='bar chart icon'></i>Reports</h5>
-                <div class='ui stackable padded grid'>
-                    
+              <div class='ui basic segment'> 
+                  <h5 class='ui header brown ribbon label'><i class='bar chart icon'></i>Reports</h5>
+                  
+                  <div class="ui hidden divider"></div>
+                 
+                  <div class="ui top attached tabular menu">
+                    <a class="active item" data-tab="first">Overall</a>
+                    <a class="item" data-tab="second">Daily</a>
+                    <a class="item" data-tab="third">Weekly</a>
+                    <a class="item" data-tab="fourth">Monthly</a>
+                  </div>
+                  <div class="ui bottom attached active tab segment" data-tab="first">
                     <!-- DIRI MAG START ANG CONTENTS SA REPORTS PAGE -->
-                    <div class='two column row'>
-                      <!-- DIRI ISUD ANG CONTENTS (e.g. graphs and shit) -->
-                      <div class='center aligned middle aligned column'>
-                        <table class='ui inverted table'>
-                          <thead>
-                            <th>PRODUCTS</th>
-                          </thead>
-                        </table>
-                        <!-- display 10 products -->
-                        <table class='ui very basic celled compact table'>
-                          <thead>
-                            <tr> 
-                              <td><b>Product Name</b></td>
-                              <td><b>Product Quantity</b></td>  
-                              <td><b>Product Total</b></td>
-                            </tr>
-                          </thead>
-                          <?php
-                          $flag = '0';
-                            foreach($data as $row){
-                              $flag = '1';
-                              ?>
-                              <tr>
-                                <td><?php echo $row->product_name; ?></td>
-                                <td><?php echo $row->quantity; ?></td>
-                                <td><?php echo $row->subtotal; ?></td>
+                    <div class='ui stackable padded grid'>  
+                      <div class='two column row'>
+                        <!-- DIRI ISUD ANG CONTENTS (e.g. graphs and shit) -->
+                        <div class='center aligned middle aligned column'>
+                          <table class='ui inverted table'>
+                            <thead>
+                              <th>PRODUCTS</th>
+                            </thead>
+                          </table>
+                          <!-- display 10 products -->
+                          <table class='ui very basic celled compact table'>
+                            <thead>
+                              <tr> 
+                                <td><b>Product Name</b></td>
+                                <td><b>Product Quantity</b></td>  
+                                <td><b>Product Total</b></td>
                               </tr>
-                              <?php
-                            }
+                            </thead>
+                            <?php
+                            $flag = '0';
+                              foreach($data as $row){
+                                $flag = '1';
+                                ?>
+                                <tr>
+                                  <td><?php echo $row->product_name; ?></td>
+                                  <td><?php echo $row->quantity; ?></td>
+                                  <td><?php echo $row->subtotal; ?></td>
+                                </tr>
+                                <?php
+                              }
 
-                            if($flag == 0){
-                              ?>
-                              <h1>Nothing to display. . .</h1>
-                              <?php
-                            }
-                          ?>
-                        </table>
-                        <a href='<?php echo site_url()?>/CReports/downloadToExcel/<?php echo $str ?>'><button class='ui circular blue icon button' title='Edit product information'>Download to Excel</button></a>
-                      </div>
-                      <div class='center aligned middle aligned column'>
-                          <div id="piechart" style="width: 500; height: 500px;"></div>
-                      </div>
-                    </div>
-
-                    <!-- if tulo ka graphs/charts in one row -->
-                    <div class='row'>
-                      <div class='five wide center aligned middle aligned column'>
-                        <!-- i display dri ang graph -->
-                        <div class='ui fluid segment'>
-                          <div id="curve_chart" ></div>
+                              if($flag == 0){
+                                ?>
+                                <h1>Nothing to display. . .</h1>
+                                <?php
+                              }
+                            ?>
+                          </table>
+                          <a href='<?php echo site_url()?>/CReports/downloadToExcel/<?php echo $str ?>'><button class='ui circular blue icon button' title='Edit product information'>Download to Excel</button></a>
                         </div>
-                          
-                      </div>
-                      <div class='five wide center aligned middle aligned column'>
-                        <!-- i display dri ang graph -->
-                        <div class='ui fluid segment'>
-                          <div id="curve_chart2"></div>
+                        <div class='center aligned middle aligned column'>
+                            <div id="piechart" style="width: 500; height: 500px;"></div>
                         </div>
-                          
                       </div>
-                      <div class='five wide center aligned middle aligned column'>
-                        <!-- i display dri ang graph -->
-                        <div class='ui fluid segment'>
-                          <div id="curve_chart3" ></div>
+                        <div class="row"></div>
+                        <div class="three wide column"></div>
+                        <div class='ten wide center aligned middle aligned column'>
+                          <!-- i display dri ang graph -->
+                          <h3 class="ui horizontal divider header">Daily Sales</h3>
+                          <div class='ui container'>
+                            <div id="curve_chart" ></div>
+                          </div>
                         </div>
-                          
-                      </div>
-                    </div>
+                        <div class="three wide column"></div>
 
-                    <!-- DIRI MAG END ANG CONTENTS SA REPORTS PAGE -->
+                        <div class="row"></div>
 
-                    <div class='row'></div> <!-- row -->
-                </div> <!-- stackable padded grid -->
-            
-            </div> <!-- segment -->
-        </div> <!-- segments -->
+                        <div class="three wide column"></div>
+                        <div class='ten wide center aligned middle aligned column'>
+                          <!-- i display dri ang graph -->
+                          <h3 class="ui horizontal divider header">Weekly Sales</h3>
+                          <div class='ui container'>
+                            <div id="curve_chart2"></div>
+                          </div>
+                        </div>
+                        <div class="three wide column"></div>
+
+                        <div class="row"></div>
+
+                        <div class="three wide column"></div>
+                        <div class='ten wide center aligned middle aligned column'>
+                          <!-- i display dri ang graph -->
+                          <h3 class="ui horizontal divider header">Monthly Sales</h3>
+                          <div class='ui container'>
+                            <div id="curve_chart3" ></div>
+                          </div>
+                        </div>
+                        <div class="three wide column"></div>
+                     
+
+                      <div class='row'></div> <!-- row -->
+                  </div> <!-- stackable padded grid -->
+                  </div>
+                  <div class="ui bottom attached tab segment" data-tab="second">
+                    <!-- DAILY CHARTS -->
+
+
+
+
+
+                  </div>
+                  <div class="ui bottom attached tab segment" data-tab="third">
+                    <!-- WEEKLY CHARTS -->
+
+
+
+
+
+                  </div>
+                  <div class="ui bottom attached tab segment" data-tab="fourth">
+                    <!-- MONTHLY CHARTS -->
+
+
+
+
+                  </div>
+
+                      
+                      
+              
+              </div> <!-- segment -->
+          </div> <!-- segments -->
 
         <!-- end of content -->
     </div> <!-- padded segment -->        
 </div> <!-- pusher -->
 
 
-
+<script>
+  $('.menu .item')
+  .tab()
+;
+</script>
 </body>
 </html>
